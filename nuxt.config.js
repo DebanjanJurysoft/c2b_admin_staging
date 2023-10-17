e16f6ed3fc7b9426136924a22c7942fad2c2e597
Apple- macbook pro <apple-macbookpro@apples-MacBook-Pro.local>
2023-10-17 14:36:33 +0530

    addons, sub-category added

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,7 +85,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3031/api',
+    baseURL: 'https://close2buy-dev.jurysoftprojects.com/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
