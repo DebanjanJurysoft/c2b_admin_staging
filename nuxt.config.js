b6fa366fa6462c7f5bb835f77569510f7263662e
Socramocharla <mocharlasocrates@gmail.com>
2023-12-16 17:47:06 +0530

    admin panel push

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,6 +91,7 @@ export default {
       },
     },
   },
+  target: 'static',
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
