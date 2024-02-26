19515c02f429b183663dbcaed271326280b87480
Debanjan <debanjan.d@juryosft.com>
2024-02-26 16:44:39 +0530

    Service page in admin panel completed

--- components/services.vue
+++ components/services.vue
@@ -28,7 +28,7 @@
                         <td>{{ service.per_month_price }}</td>
                         <td class="d-flex justify-content-center lign-items-center gap10">
                             <button class="button" @click.prevent="openEdit(service)"><i class="fa fa-pencil"></i></button>
-                            <button class="button"><i class="fa fa-trash"></i></button>
+                            <button class="button" @click.prevent="removeService(service.id)"><i class="fa fa-trash"></i></button>
                         </td>
                     </tr>
                 </tbody>
@@ -45,6 +45,32 @@
             <div class="d-flex justify-content-between align-items-center">
                 <span>{{ singleData.id ? 'Update Service' : 'Create New Service' }}</span>
                 <button class="button" @click.prevent="showHideModal(false, 'services')"><i class="fa fa-times"></i></button>
+            </div>
+            <div class="d-flex flex-column py-3">
+                <div class="d-flex flex-column">
+                    <span class="input-label">Service Name:</span>
+                    <b-form-input v-model="singleData.service_name" placeholder="Service Name"></b-form-input>
+                </div>
+                <div class="d-flex flex-column">
+                    <span class="input-label">Per Month Price:</span>
+                    <b-form-input v-model="singleData.per_month_price" placeholder="Per Month Price"></b-form-input>
+                </div>
+                <div class="d-flex flex-column">
+                    <span class="input-label">Per Day Price:</span>
+                    <b-form-input v-model="singleData.per_day_price" placeholder="Per Day Price"></b-form-input>
+                </div>
+                <div class="d-flex flex-column">
+                    <span class="input-label">Service Image:</span>
+                    <b-form-file @input="handleFileInput($event)" v-model="singleData.file2" class="mt-3" plain></b-form-file>
+                    <div v-if="singleData.image" class="d-flex flex-column align-items-center">
+                        <img :src="singleData.image" alt="Image" width="100" height="100">
+                        <button @click.prevent="clearFile" class="button"><i class="fa fa-trash"></i></button>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center gap10 pt-3">
+                    <button class="button" @click.prevent="saveServices"><i class="fa fa-save mr-2"></i>SAVE</button>
+                    <button class="button" @click.prevent="showHideModal(false, 'services')"><i class="fa fa-ban mr-2"></i>CLOSE</button>
+                </div>
             </div>
         </div>
         </b-modal>
@@ -78,6 +104,7 @@ export default {
                 id: null,
                 service_name: null,
                 image: null,
+                file2: null,
                 per_day_price: null,
                 per_month_price: null,
             }
@@ -109,6 +136,132 @@ export default {
             this.singleData.per_month_price = data.per_month_price
             this.singleData.image = data.image
             this.showHideModal(true, 'services')
+        },
+        handleFileInput(event) {
+            if (event) {
+                this.singleData.image = URL.createObjectURL(event)
+            }
+        },
+        clearFile() {
+            this.singleData.image = null
+            this.singleData.file2 = null
+        },
+        async saveServices() {
+            if (!this.singleData.service_name || !this.singleData.service_name.trim()) {
+                this.$toast.show('Enter a service name.', {
+                    duration: 1500,
+                    position: 'top-right',
+                    keepOnHover: true,
+                    type: 'error'
+                })
+                return
+            }
+            if (!this.singleData.per_day_price || !`${this.singleData.per_day_price}`.trim()) {
+                this.$toast.show('Enter a service cost per day.', {
+                    duration: 1500,
+                    position: 'top-right',
+                    keepOnHover: true,
+                    type: 'error'
+                })
+                return
+            }
+            if (!this.singleData.per_month_price || !`${this.singleData.per_month_price}`.trim()) {
+                this.$toast.show('Enter a service cost per month.', {
+                    duration: 1500,
+                    position: 'top-right',
+                    keepOnHover: true,
+                    type: 'error'
+                })
+                return
+            }
+            if ((!this.singleData.image || !this.singleData.image.trim()) && !this.singleData.file2) {
+                this.$toast.show('Select an image for the service.', {
+                    duration: 1500,
+                    position: 'top-right',
+                    keepOnHover: true,
+                    type: 'error'
+                })
+                return
+            }
+            let url = !this.singleData.id ? '/services/add-new-services' : '/services/update-services'
+            const form = new FormData()
+            form.append('service_name', this.singleData.service_name)
+            if (this.singleData.file2) {
+                form.append('image', this.singleData.file2)
+            }
+            form.append('per_day_price', this.singleData.per_day_price)
+            form.append('per_month_price', this.singleData.per_month_price)
+            if (this.singleData.id) {
+                form.append('service_id', this.singleData.id)
+            }
+            const response = await this.$axios({
+                method: 'POST',
+                url,
+                data: form
+            })
+            this.$toast.show(response.data.message, {
+                duration: 1500,
+                position: 'top-right',
+                keepOnHover: true,
+                type: response.data.status
+            })
+            await this.fetchServices()
+            this.showHideModal(false, 'services')
+        },
+        async removeService(id) {
+            this.deleteService('/services/delete-service', 'service_id', id)
+        },
+        async deleteService(path, key, data_id) {
+            try {
+                const h = this.$createElement
+                const id = `DeleteModal${id}`
+                this.$bvToast.hide(id)
+                const $closeButton = h(
+                'b-button',
+                {
+                    on: { click: () => this.$bvToast.hide(id) },
+                    class: 'btn btn-primary mx-1',
+                    style: 'margin: 0 auto;'
+                },
+                'No'
+                )
+                const $acceptButton = h(
+                'b-button',
+                {
+                    on: {
+                        click: async () => {
+                            let data = {}
+                            data[key] = data_id
+                            const deleteresponse = await this.$axios.post(path, data)
+                            this.$toast.show(deleteresponse.data.message, {
+                                duration: 1500,
+                                position: 'top-right',
+                                keepOnHover: true,
+                                type: deleteresponse.data.status
+                            })
+                            await this.fetchServices()
+                            this.$bvToast.hide(id)
+                        }
+                    },
+                    class: 'btn btn-danger mx-1',
+                },
+                'Yes'
+                )
+                const $buttonContainer = h(
+                    'div',
+                    {
+                        class: 'text-center my-2',
+                    },
+                    [$closeButton, $acceptButton]
+                )
+                this.$bvToast.toast($buttonContainer, {
+                    id: id,
+                    title: `Are you sure?`,
+                    noCloseButton: true,
+                })
+            } catch (error) {
+                console.log(error);
+            }
         },
         async fetchServices() {
             const response = await this.$axios.get('/services/fetch-all-services')
