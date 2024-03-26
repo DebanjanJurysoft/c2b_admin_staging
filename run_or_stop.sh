4cbb8cf3d2716543938bc829f78f5b613f6a6207
Debanjan <debanjan.d@juryosft.com>
2024-03-26 18:51:29 +0530

    Shel;l added

---
+++ run_or_stop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <action>"
+    exit 1
+fi
+
+# Extract the direction parameter
+action="$1"
+
+# Perform actions based on the provided direction
+case "$action" in
+    "run")
+        echo "Starting containers"
+        docker-compose up -d --build
+        ;;
+    "stop")
+        echo "Stopping containers"
+        docker-compose down
+        ;;
+    *)
+        echo "Invalid action. Please specify 'run' or 'stop'."
+        exit 1
+        ;;
+esac
+
+exit 0
